2462c95368abd84c39c8fd9441b007bc5b7cb2aa
AnjalBam <anjalbam81@gmail.com>
2020-10-02 19:10:50 +0545

    feat: implement quicksort in java

--- Algorithms/java/com.Algorithms/QuickSort.java
+++ Algorithms/java/com.Algorithms/QuickSort.java
@@ -5,7 +5,7 @@
 public class QuickSort {
 
   int partition(int myArr[], int low, int high) {
-    int pivotEl = myArr[high];  // Takes last element as pivot element
+    int pivotEl = myArr[high]; // Takes last element as pivot element
     int i = low - 1;
 
     for (int j = low; j < high; j++) {
@@ -26,7 +26,32 @@ public class QuickSort {
 
     return i + 1;
   }
+  
+  void sort(int myArr[], int low, int high) {
+    if (low < high) {
+      // pi is the Partition index arr[pi] is now at right place.
+      int pi = partition(myArr, low, high);
+      sort(myArr, low, pi - 1);
+      sort(myArr, pi + 1, high);
+    }
+  }
+
+  static void printArray(int myArr[]) {
+    int arrLen = myArr.length;
+    for (int i = 0; i < arrLen; i++) {
+      System.out.print(myArr[i] + " ");
+    }
+    System.out.println("");
+  }
+
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    // System.out.println("Hello World!");
+    int unsortedArr[] = { 10, 4, 11, 5, 8 };
+    int n = unsortedArr.length;
+    QuickSort qsOb = new QuickSort();
+    qsOb.sort(unsortedArr, 0, n - 1);
+
+    System.out.println("SortedArray: ");
+    printArray(unsortedArr);
   }
 }
