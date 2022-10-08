cffe823a8304ead2900f36a0fb230d78d79dd5ae
Dmitriy S <contact@dhotspot.xyz>
2022-10-07 20:16:44 -0400

    Creation of BMICalc.java
    This program is able to calculate your body mass index with the information you provide including your height and weight.

---
+++ Programs/Java/BMICalc.java
@@ -0,0 +1,26 @@
+/*
+ * Author: DmitriyShum
+ * Date: 10/7/22
+ * Title: BMICalculator
+ * Desc: This calculator can be used to find your body mass index by using a simple calculation which is:
+ *  703 * weight(in LBS)/height(in inches) to the second power.
+ */
+import java.util.Scanner;
+import java.math.*;
+public class BMICalc{
+    public static float height;//in Inches.
+    public static float weight;//in LBS(Pounds).
+
+    public static void main(String[] args){
+        Scanner info = new Scanner(System.in);
+        System.out.println("Please enter your height in inches :  ");
+        height = info.nextFloat();
+        System.out.println("Please enter your weight in pounds(lbs) : ");
+        weight = info.nextFloat();
+        //Output calculation.
+        System.out.println("Your BMI(Body Mass Index) is " + 703 * weight/Math.pow(height, 2) + ".");
+
+        //close scanner
+        info.close();
+    }
+}
